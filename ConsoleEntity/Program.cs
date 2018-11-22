405f7b582ffe94db439c3fd821468075833b9e5b
Sundeep <sundeepm1@gmail.com>
2018-11-22 11:27:49 +0530

    initial commit

---
+++ ConsoleEntity/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleEntity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new MusicStoreDBContext())
+            {
+                var albums = context.Albums;
+                //Album a1 = new Album {
+                //    Title = "Album2", 
+                //    Price = 20
+                //};
+                //context.Albums.Add(a1);
+                //context.SaveChanges();
+
+                Console.WriteLine(albums.Count());
+
+                Console.WriteLine(context.Albums.Where(x => x.Title == "Album1").Count());
+                Console.ReadKey();
+            }
+        }
+    }
+}
